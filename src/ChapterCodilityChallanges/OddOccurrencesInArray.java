d16e4721f5bc5341f9fbd100160a57a3bb408ac3
Wiktor Religo <wiciorny@gmail.com>
2020-12-09 23:54:26 +0100

    working with new lesson challange, not finished yet, first test passed

--- src/ChapterCodilityChallanges/OddOccurrencesInArray.java
+++ src/ChapterCodilityChallanges/OddOccurrencesInArray.java
@@ -1,8 +1,9 @@
 package ChapterCodilityChallanges;
 
 
-import java.util.HashSet;
-import java.util.Set;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  *
@@ -11,16 +12,38 @@ public class OddOccurrencesInArray {
 
 
     public static void main(String[] args) {
-        Set<Integer> stringSet = new HashSet<>();
-        Integer[] arr = new Integer[];
 
-        for(int i=0; i<arr.length; i++){
-            if(stringSet.add(arr[i])){
 
-            }else{
-                stringSet.remove(arr[i]);
+    }
+        public int solutionOddOccurrences(int[] A){
+            int counter = -1 ;
+            int unique = -1;
+            for (int i = 0; i < A.length; i++) {
+
+                for (int j = i+1; j < A.length; j++) {
+                    System.out.println("i "+ i + "j" + j );
+                    if (A[i] == A[j]) {
+                      //  System.out.println("times" + j );
+                        counter=1;
+                        break;
+                    }
+                }
+                if( counter != -1){
+                  counter= -1;
+                    System.out.println("test");
+                }else  {
+                    System.out.println("else");
+                    unique=A[i];
+                }
             }
+            return unique;
         }
 
+
+    @Test
+    public void testOccurernces(){
+       assertEquals(12,new OddOccurrencesInArray().solutionOddOccurrences(new int[]{11,10,10,9,2,2,9,7,7,9,11,12}));
+//       assertTrue();
+//       assertTrue();
     }
 }
