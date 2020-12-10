c262b95db2c02cafae3443df930caf995dcb6f83
Wiktor Religo <wiciorny@gmail.com>
2020-12-10 22:13:22 +0100

    adding two solution for OddOccurrence in Array - 100% example with bit operator xor

--- src/ChapterCodilityChallanges/CyclingRotation.java
+++ src/ChapterCodilityChallanges/CyclingRotation.java
@@ -1,49 +1,44 @@
 package ChapterCodilityChallanges;
 
-
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+
 
 /**
  *
  */
 public class CyclingRotation {
 
-
     public static void main(String[] args) {
-        new CyclingRotation().solution(new int[]{1,2,4,3},3);
-        new CyclingRotation().solution(new int[]{3,8,9,7,6},91);
+        new CyclingRotation().solution(new int[]{1, 2, 4, 3}, 3);
+        new CyclingRotation().solution(new int[]{3, 8, 9, 7, 6}, 91);
     }
 
 
-    public int[] solution(int[] A, int K){
+    public int[] solution(int[] A, int K) {
         int lengthArr = A.length;
-        if(K == lengthArr){
+        if (K == lengthArr) {
             return A;
         }
-        int []B = new int[A.length];
-        for(int i=0;i<A.length;i++){
-            if(i+K < lengthArr) {
+        int[] B = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            if (i + K < lengthArr) {
                 B[i + K] = A[i];
-            }
-            else{
-                B[(i+K)%lengthArr]= A[i];
+            } else {
+                B[(i + K) % lengthArr] = A[i];
             }
         }
         return B;
     }
 
     @Test
-    public void testSolutionCycling(){
-        int[] array1 = new int[]{1,3,6,4,1,2};
-        int[] array2 = new int[]{1,2,3};
+    public void testSolutionCycling() {
+        int[] array1 = new int[]{1, 3, 6, 4, 1, 2};
+        int[] array2 = new int[]{1, 2, 3};
 
-        assertArrayEquals(array2, new CyclingRotation().solution(array2,3));
-        assertArrayEquals(new int[]{9, 7, 6, 3, 8}, new CyclingRotation().solution(new int[]{3,8,9,7,6},3));
-        assertArrayEquals(new int[]{2, 1, 3, 6, 4,1}, new CyclingRotation().solution(array1,31));
+        assertArrayEquals(array2, new CyclingRotation().solution(array2, 3));
+        assertArrayEquals(new int[]{9, 7, 6, 3, 8}, new CyclingRotation().solution(new int[]{3, 8, 9, 7, 6}, 3));
+        assertArrayEquals(new int[]{2, 1, 3, 6, 4, 1}, new CyclingRotation().solution(array1, 31));
     }
 }
--- src/ChapterCodilityChallanges/OddOccurrencesInArray.java
+++ src/ChapterCodilityChallanges/OddOccurrencesInArray.java
@@ -3,10 +3,18 @@ package ChapterCodilityChallanges;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- *
+ *Two types of implemetation using XOR - go check reference of bit XOR OPERATION
+ * 1 - 1 => 0
+ * 1 - 0 => 1
+ * 0 - 1 => 1
+ * 0 - 0 => 0
+ * 100% solution for codility problem Lesson
  */
 public class OddOccurrencesInArray {
 
@@ -15,35 +23,40 @@ public class OddOccurrencesInArray {
 
 
     }
-        public int solutionOddOccurrences(int[] A){
-            int counter = -1 ;
-            int unique = -1;
-            for (int i = 0; i < A.length; i++) {
 
-                for (int j = i+1; j < A.length; j++) {
-                    System.out.println("i "+ i + "j" + j );
-                    if (A[i] == A[j]) {
-                      //  System.out.println("times" + j );
-                        counter=1;
-                        break;
-                    }
-                }
-                if( counter != -1){
-                  counter= -1;
-                    System.out.println("test");
-                }else  {
-                    System.out.println("else");
-                    unique=A[i];
-                }
+    public int soultionWithSet(int[] A) {
+        Set<Integer> integerSet = new HashSet<>();
+        for (int a : A) {
+            if (!integerSet.contains(a)) {
+                integerSet.add(a);
+            } else {
+                integerSet.remove(a);
             }
-            return unique;
         }
+        System.out.println(integerSet);
+        return integerSet.iterator()
+                .next();
+    }
+
+    public int solutionOddOccurrences(int[] A) {
+        int result = 0;
+        for (int value : A) {
+            result ^= value;
+        }
+        return result;
+    }
 
 
     @Test
-    public void testOccurernces(){
-       assertEquals(12,new OddOccurrencesInArray().solutionOddOccurrences(new int[]{11,10,10,9,2,2,9,7,7,9,11,12}));
-//       assertTrue();
-//       assertTrue();
+    public void testOccurernces() {
+        assertEquals(12,
+                new OddOccurrencesInArray().soultionWithSet(new int[]{11, 9, 10, 10, 9, 2, 2, 9, 7, 7, 9, 11, 12}));
+        assertEquals(5,
+                new OddOccurrencesInArray().soultionWithSet(new int[]{11, 10, 10, 11, 5, 9, 2, 2, 9, 7, 7, 12, 9, 12}));
+        assertEquals(1, new OddOccurrencesInArray().soultionWithSet(
+                new int[]{11, 1, 11, 10, 10, 9, 2, 2, 9, 7, 7, 9, 11, 12, 12}));
+        assertEquals(1, new OddOccurrencesInArray().solutionOddOccurrences(
+                new int[]{11, 1, 10, 10, 9, 2, 2, 9, 7, 7, 9, 9, 11, 12, 12}));
+        assertEquals(1, new OddOccurrencesInArray().solutionOddOccurrences(new int[]{11, 1, 11,}));
     }
 }
