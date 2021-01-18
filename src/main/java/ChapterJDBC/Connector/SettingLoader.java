7ecdc41e597959d78221d11ebd3fa1967fefdd4a
Wiktor Religo <wiciorny@gmail.com>
2021-01-18 22:33:26 +0100

    settingLoader config

--- src/main/java/ChapterJDBC/Connector/SettingLoader.java
+++ src/main/java/ChapterJDBC/Connector/SettingLoader.java
@@ -1,11 +1,39 @@
 package ChapterJDBC.Connector;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-public  class SettingLoader extends JDBConnector{
+public class SettingLoader {
 
+    private static Properties properties = new Properties();
+    private String url;
+    private String userName;
+    private String password;
 
+    public static void loadSettingsFromFile(String fileName) throws IOException {
+        properties.load(new FileInputStream(fileName));
+        new SettingLoader().setDatabaseConfig();
+    }
 
+    private void setDatabaseConfig() {
+        url= properties.getProperty("database.url");
+        userName= properties.getProperty("database.host");
+        password = properties.getProperty("database.password");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 
 }
 
